464e2d82d3280a351bb1d209b31d26ec624b90c1
“sungjunkim” <kis07150@naver.com>
2024-01-13 12:32:02 +0900

    [DOK-143] feat : 스타일컴포넌트 사용 예시 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useStore from "./store/store";
-import { BoxOne, BoxTwo, Wrapper, Box } from "./common";
+import { BoxOne, BoxTwo, Wrapper, Box, CommonCircle, AnimationCircle, SpanBox } from "./common";
 
 const App = () => {
   const { bears, increasePopulation, removeAllBears } = useStore(
@@ -11,10 +11,15 @@ const App = () => {
     <>
       <Wrapper>
         <Box backgroundcolor={"#cf6a87"} />
-        <Box backgroundcolor={"#574b90"} />
-        {/* <BoxOne/>
-        <BoxTwo/> */}
+        <Box as="button" backgroundcolor={"#574b90"} onClick={() => alert("버튼이 클릭되었습니다.")}/> {/* as 를 사용해서 div를 button 속성으로 바꿀수 있다. */}
+        <CommonCircle backgroundcolor={"black"} />
+        <AnimationCircle />
+        <SpanBox backgroundcolor={"#cf6a87"}>
+          <span>😄</span>
+        </SpanBox>
       </Wrapper>
+        
+      
       <h1>{bears} around here ...</h1>
       <button onClick={increasePopulation}>one up</button>
       <button onClick={removeAllBears}>remove all</button>
