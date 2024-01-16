778b3eb266e4bf71a09a0b6a91c7837eae9cde52
“sungjunkim” <kis07150@naver.com>
2024-01-16 09:54:11 +0900

    [DOK-143] refactor : 코드 스멜 제거 및 prettier 수행

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,13 +1,13 @@
-import { Card } from "./Card"
+import { Card } from "./Card";
 import React from "react";
 
 const Grid = () => {
   return (
     <div>
-        <Card />
-        Grid
+      <Card />
+      Grid
     </div>
-  )
+  );
 };
 
 export default Grid;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
-  </>,
+  <ThemeProvider theme={theme}>
+    <Router />
+  </ThemeProvider>
 );
