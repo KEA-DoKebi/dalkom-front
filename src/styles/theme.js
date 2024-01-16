64c86b4f690ebf8d16258e302491dad27b96f3d4
ms987796 <ms987796@naver.com>
2024-01-16 10:13:15 +0900

    feat: 디자인 가이드 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,6 +9,20 @@ const fontSizes = {
   xxl: calcRem(22),
   xxxl: calcRem(24),
   titleSize: calcRem(50),
+
+  /* 아래 사이즈는 주혜님 디자인 가이드 참고 */
+  small: calcRem(10),
+  base: calcRem(15),
+  lg: calcRem(20),
+  xl: calcRem(25),
+  xxl: calcRem(30),
+  xxxl: calcRem(40),
+  titleSize: calcRem(50),
+};
+
+const font = {
+  'font-family': 'Eighteous',
+  src: "url('./font/Righteous-Regular.woff') format('woff')"
 };
 
 const paddings = {
@@ -53,7 +67,51 @@ const colors = {
   white: "#FFFFFF",
   gray_1: "#222222",
   gray_2: "#767676",
+  gray_3: "#E0E0E0",
   green_1: "#3cb46e",
+  yellow_1: "#FFF9C4",
+  yellow_2: "#FFF59D",
+  yellow_3: "#FFD465",
+  yellow_4: "#FFE082",
+    // primary
+  $primaryLight: "#E4E7EC",
+  $primaryMain: "#909AB0",
+  $primaryDark: "#203461",
+  $primary200: "#1C2F59",
+  $primary800: "#132145",
+  // secondary
+  $secondaryLight: "#FDE8EF",
+  $secondaryMain: "#F6A0BD",
+  $secondaryDark: "#EC407A",
+  $secondary200: "#EA3A72",
+  $secondary800: "#E42A5D",
+  // success Colors
+  $successLight: "#B9F6CA",
+  $success200: "#69F0AE",
+  $successMain: "#00E676",
+  $successDark: "#00C853",
+  // error
+  $errorLight: "#EF9A9A",
+  $errorMain: "#F44336",
+  $errorDark: "#C62828",
+  // orange
+  $orangeLight: "#FBE9E7",
+  $orangeMain: "#FFAB91",
+  $orangeDark: "#D84315",
+  // warning
+  $warningLight: "#FFF8E1",
+  $warningMain: "#FFE57F",
+  $warningDark: "#FFC107",
+  // grey
+  $pureWhite: "#FFFFFF",
+  $grey50: "#F8FAFC",
+  $grey100: "#EEF2F6",
+  $grey200: "#E3E8EF",
+  $grey300: "#CDD5DF",
+  $grey500: "#697586",
+  $grey600: "#4B5565",
+  $grey700: "#364152",
+  $grey900: "#121926",
 };
 
 const device = {
@@ -76,3 +134,4 @@ const theme = {
 };
 
 export default theme;
+
