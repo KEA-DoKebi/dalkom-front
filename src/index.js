69ada07dd2e66fed55e35a3c5ae2c6674b06e6ea
“sungjunkim” <kis07150@naver.com>
2024-01-16 09:59:37 +0900

    [DOK-143] bug : 파일명 변경으로 인한 임포트 오류 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import Router from "./Router";
-import theme from "./styles/theme";
+import theme from "./styles/commonTheme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
