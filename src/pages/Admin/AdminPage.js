e38f48c2c034584dd2cb439f4e927f72d8a823b7
juhye87 <78808933+juhye87@users.noreply.github.com>
2024-01-18 14:43:41 +0900

    [DOK-186] design : 관리자페이지 틀 잡는중

--- src/pages/Admin/AdminPage.js
+++ src/pages/Admin/AdminPage.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Box } from "@mui/system";
+import { Grid, Paper, Toolbar } from "@mui/material";
+import AdminBar from "../../components/AdminBar";
+import { Input } from "@mui/joy";
 
-const AdminPage = () => {
+export default function AdminPage() {
   return (
-    <>
-      <Link to="/">메인페이지로 이동</Link>
-    </>
+    <Paper
+      sx={{ display: "flex", height: "100vh", backgroundColor: "#EEF2F6" }}
+    >
+      <AdminBar />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          backgroundColor: "#EEF2F6",
+          flexGrow: 1,
+        }}
+      >
+        <Toolbar />
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            backgroundColor: "#FFFFFF",
+            borderRadius: "27px",
+            margin: "16px",
+          }}
+        >
+          <Grid>
+            
+          </Grid>
+        </Box>
+      </Box>
+    </Paper>
   );
-};
+}
 
-export default AdminPage;
+
+
+
+
+
