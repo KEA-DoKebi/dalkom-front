8fd49fcba2b143e175313f012b2e4a442f90e4d1
“sungjunkim” <kis07150@naver.com>
2024-01-18 10:55:17 +0900

    [DOK-143] test

--- src/pages/User/ProductList.js
+++ src/pages/User/ProductList.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const ProductList = () => {
+  return (
+    <SidebarLayout>
+      <CardGrid />
+    </SidebarLayout>
+
+    
+  );
   return <div>ProductList</div>;
 };
 
