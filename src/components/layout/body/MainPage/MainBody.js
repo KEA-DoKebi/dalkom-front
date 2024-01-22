40d693aca398fc04b1163c24b6355559f994e750
“sungjunkim” <kis07150@naver.com>
2024-01-22 20:43:04 +0900

    [DOK-204] pull : dev

--- src/components/layout/body/MainPage/MainBody.js
+++ src/components/layout/body/MainPage/MainBody.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Box, Avatar, Typography } from "@mui/material";
+import { Paper, Box, Avatar, Typography, Divider } from "@mui/material";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 // import KaKaoFriendsImage from "/images/kakaofriends/webp"
@@ -9,6 +9,8 @@ import { Link } from "react-router-dom";
 const MainBody = () => {
   return (
     <StyledBox>
+      {/* 배너 */}
+      <Divider />
       <StyledCarousel>
         <StyledPaper sx={{ backgroundColor: "#F5F7FB" }}>
           <Link to="/category/1">
@@ -22,32 +24,29 @@ const MainBody = () => {
           </Link>
         </StyledPaper>
       </StyledCarousel>
+      
+      {/* 카테고리 목록  */}
       <CategoryBox>
         <ImageBox>
           <StyledAvartar src="/images/MainPage/hanger.png" />
           <Typography>패션/뷰티</Typography>
         </ImageBox>
-
         <ImageBox>
           <StyledAvartar src="/images/MainPage/life.png" />
           <Typography>생활</Typography>
         </ImageBox>
-
         <ImageBox>
           <StyledAvartar src="/images/MainPage/digital.png" />
           <Typography>디지털/가전</Typography>
         </ImageBox>
-
         <ImageBox>
           <StyledAvartar src="/images/MainPage/baby.png" />
           <Typography>출산/유아동</Typography>
         </ImageBox>
-
         <ImageBox>
           <StyledAvartar src="/images/MainPage/sports.png" />
           <Typography>스포츠/레저</Typography>
         </ImageBox>
-
         <ImageBox>
           <StyledAvartar src="/images/MainPage/chunsik2.png" />
           <Typography>카카오굿즈</Typography>
@@ -70,7 +69,7 @@ const StyledBox = styled(Box)`
 const StyledCarousel = styled(Carousel)`
   justify-content: center;
   align-items: center;
-  // margin-top : -5vh;
+  z-index : 0;
 `;
 
 const StyledPaper = styled(Paper)`
@@ -94,7 +93,7 @@ const ImageBox = styled(Box)`
 const StyledAvartar = styled(Avatar)`
   width: 100px;
   height: 100px;
-  margin: 40px 50px 10px 50px;
+  margin: 25px 50px;
   background-color: #f5f7fb;
   border: 1px solid gray;
 `;
