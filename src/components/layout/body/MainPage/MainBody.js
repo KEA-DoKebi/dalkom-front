d54f77e2ac8968174fa06249a4bfc15a167fdcbb
“sungjunkim” <kis07150@naver.com>
2024-01-22 17:17:15 +0900

    [DOK-185] build : 안쓰는 코드 수정

--- src/components/layout/body/MainPage/MainBody.js
+++ src/components/layout/body/MainPage/MainBody.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Box } from "@mui/material";
+import { Paper } from "@mui/material";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 // import KaKaoFriendsImage from "/images/kakaofriends/webp"
