4aaf76de26ef99c1abbe5081511a848aa79d2828
“sungjunkim” <kis07150@naver.com>
2024-01-23 11:43:47 +0900

    [DOK-204] fix : 탑바, 배너 라우팅 수정

--- src/components/layout/body/MainPage/MainBody.js
+++ src/components/layout/body/MainPage/MainBody.js
@@ -20,260 +20,6 @@ import axios from "axios";
 import { ProductCard } from "components/ProductCard";
 
 // MainBody코드
-// const MainBody = () => {
-//   const [clothesProductList, setClothesProductList] = useState([]);
-//   const [lifesProductList, setLifesProductList] = useState([]);
-//   const [digitalProductList, setDigitalProductList] = useState([]);
-//   const [babyProductList, setBabyProductList] = useState([]);
-//   const [sportsProductList, setSportsProductList] = useState([]);
-//   const [kakaofriendsProductList, setKakaofriendsProductList] = useState([]);
-
-//   const getMainProductList = async () => {
-//     const res = await axios.get("/data/productData/mainPageProduct.json");
-//     // console.log(res.data);
-//     // setMainProductList(res.data);
-//     setClothesProductList(res.data.clothes);
-//     setLifesProductList(res.data.life);
-//     setDigitalProductList(res.data.digital);
-//     setBabyProductList(res.data.baby);
-//     setSportsProductList(res.data.sports);
-//     setKakaofriendsProductList(res.data.kakaofriends);
-//   };
-
-//   useEffect(() => {
-//     getMainProductList();
-//   }, []);
-
-//   return (
-//     <StyledBox>
-//       {/* 배너 */}
-//       <Divider />
-//       <StyledCarousel>
-//         <StyledPaper sx={{ backgroundColor: "#FFF8DC" }}>
-//           <Link to="/category/1">
-//             <BannerImage src="/images/MainPage/dokebiBanner1.png" />
-//           </Link>
-//         </StyledPaper>
-
-//         <StyledPaper sx={{ backgroundColor: "#FEE300" }}>
-//           <Link to="/category/1">
-//             <BannerImage src="/images/MainPage/dokebiBanner2.png" />
-//           </Link>
-//         </StyledPaper>
-
-//         <StyledPaper sx={{ backgroundColor: "#494881" }}>
-//           <Link to="/category/1">
-//             <BannerImage src="/images/MainPage/dokebiBanner3.png" />
-//           </Link>
-//         </StyledPaper>
-//       </StyledCarousel>
-
-//       {/* 카테고리 목록  */}
-      // <CategoryBox>
-      //   <Link to="/catagory/fashion">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/hanger.png" />
-      //       <Typography>패션/뷰티</Typography>
-      //     </ImageBox>
-      //   </Link>
-      //   <Link to="/catagory/life">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/life.png" />
-      //       <Typography>생활</Typography>
-      //     </ImageBox>
-      //   </Link>
-      //   <Link to="/catagory/digital">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/digital.png" />
-      //       <Typography>디지털/가전</Typography>
-      //     </ImageBox>
-      //   </Link>
-      //   <Link to="/catagory/baby">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/baby.png" />
-      //       <Typography>출산/유아동</Typography>
-      //     </ImageBox>
-      //   </Link>
-      //   <Link to="/catagory/sports">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/sports.png" />
-      //       <Typography>스포츠/레저</Typography>
-      //     </ImageBox>
-      //   </Link>
-      //   <Link to="/catagory/kakaofriends">
-      //     <ImageBox>
-      //       <StyledAvartar src="/images/MainPage/chunsik2.png" />
-      //       <Typography>카카오굿즈</Typography>
-      //     </ImageBox>
-      //   </Link>
-      // </CategoryBox>
-
-//       {/* 카테고리별 상품 목록 */}
-//       <TableContainer component={Paper}>
-//         <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//           <TableHead>
-//             <TableRow sx={{ display: "flex", justifyContent: "center" }}>
-//               <StyleTypoGrapy>상품 목록</StyleTypoGrapy>  
-//             </TableRow>
-//           </TableHead>
-//           <TableBody sx={{margin : 0, padding : 0}}>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>의류</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {clothesProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>생활</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {lifesProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>디지털</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {digitalProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>유아</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {babyProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>스포츠</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {sportsProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//             <TableRow>
-//               <Grid container spacing={1}>
-//                 <CenterGrid item xs={3}>
-//                   <StyleTypoGrapy>카카오프렌즈</StyleTypoGrapy>
-//                 </CenterGrid>
-//                 <Grid item xs={9}>
-//                   <Grid container spacing={3} mt={5}>
-//                     {kakaofriendsProductList.map((v) => {
-//                       return (
-//                         <Grid item xs={3} key={v.productNum}>
-//                           <ProductCard
-//                             imageUrl={v.imageUrl}
-//                             title={v.productName}
-//                             price={v.productPrice}
-//                             star={v.productStar}
-//                             review={v.productReview}
-//                             seq={v.productSeq}
-//                           />
-//                         </Grid>
-//                       );
-//                     })}
-//                   </Grid>
-//                 </Grid>
-//               </Grid>
-//             </TableRow>
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     </StyledBox>
-//   );
-// };
-
 const MainBody = () => {
   const [productLists, setProductLists] = useState({
     clothes: [],
@@ -297,23 +43,23 @@ const MainBody = () => {
     <StyledBox>
       <Divider />
       <StyledCarousel>
-      <StyledPaper sx={{ backgroundColor: "#FFF8DC" }}>
-           <Link to="/category/1">
-             <BannerImage src="/images/MainPage/dokebiBanner1.png" />
-           </Link>
-         </StyledPaper>
+        <StyledPaper sx={{ backgroundColor: "#FFF8DC" }}>
+          <Link to="/cs/manual">
+            <BannerImage src="/images/MainPage/dokebiBanner1.png" />
+          </Link>
+        </StyledPaper>
 
-         <StyledPaper sx={{ backgroundColor: "#FEE300" }}>
-           <Link to="/category/1">
-             <BannerImage src="/images/MainPage/dokebiBanner2.png" />
-           </Link>
-         </StyledPaper>
+        <StyledPaper sx={{ backgroundColor: "#FEE300" }}>
+          <Link to="/cs/manual">
+            <BannerImage src="/images/MainPage/dokebiBanner2.png" />
+          </Link>
+        </StyledPaper>
 
-         <StyledPaper sx={{ backgroundColor: "#494881" }}>
-           <Link to="/category/1">
-             <BannerImage src="/images/MainPage/dokebiBanner3.png" />
-           </Link>
-         </StyledPaper>
+        <StyledPaper sx={{ backgroundColor: "#494881" }}>
+          <Link to="/cs/manual">
+            <BannerImage src="/images/MainPage/dokebiBanner3.png" />
+          </Link>
+        </StyledPaper>
       </StyledCarousel>
 
       <CategoryBox>
@@ -363,34 +109,36 @@ const MainBody = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {Object.entries(productLists).map(([category, productList], index) => (
-              <TableRow key={index}>
-                <TableCell>
-                <Grid container spacing={1}>
-                  <CenterGrid item xs={3}>
-                    <StyleTypoGrapy>{category}</StyleTypoGrapy>
-                  </CenterGrid>
-                  <Grid item xs={9}>
-                    <Grid container spacing={3}>
-                      {productList.map((product) => (
-                        <Grid item xs={3} key={product.productSeq}>
-                          <ProductCard
-                            key = {product.productSeq}
-                            imageUrl={product.imageUrl}
-                            title={product.productName}
-                            price={product.productPrice}
-                            star={product.productStar}
-                            review={product.productReview}
-                            seq={product.productSeq}
-                          />
+            {Object.entries(productLists).map(
+              ([category, productList], index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <Grid container spacing={1}>
+                      <CenterGrid item xs={3}>
+                        <StyleTypoGrapy>{category}</StyleTypoGrapy>
+                      </CenterGrid>
+                      <Grid item xs={9}>
+                        <Grid container spacing={3}>
+                          {productList.map((product) => (
+                            <Grid item xs={3} key={product.productSeq}>
+                              <ProductCard
+                                key={product.productSeq}
+                                imageUrl={product.imageUrl}
+                                title={product.productName}
+                                price={product.productPrice}
+                                star={product.productStar}
+                                review={product.productReview}
+                                seq={product.productSeq}
+                              />
+                            </Grid>
+                          ))}
                         </Grid>
-                      ))}
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </Grid>
-                </TableCell>
-              </TableRow>
-            ))}
+                  </TableCell>
+                </TableRow>
+              ),
+            )}
           </TableBody>
         </Table>
       </TableContainer>
@@ -447,7 +195,7 @@ const StyledLink = styled(Link)`
   &:hover {
     color: black; // 호버 상태에서도 색상 유지
   }
-`
+`;
 
 const CenterGrid = styled(Grid)`
   ${centerFlex}
