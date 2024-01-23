d683d407480891cfd1da5d85c1c985d256d96c06
suha <hp980724@gmail.com>
2024-01-23 10:20:17 +0900

    feat : 문의와 안내 모달 추가 기능

--- src/pages/Admin/CSManage/AnnouncementPage.js
+++ src/pages/Admin/CSManage/AnnouncementPage.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { AdminButton, InputBoxS, InputBoxM } from "../../../components/AdminComponents";
+import {
+  AdminButton,
+  InputBoxS,
+  InputBoxM,
+  CustomSelect,
+} from "../../../components/AdminComponents";
 import AdminBar from "../../../components/AdminBar";
 import SearchIcon from "@mui/icons-material/Search";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
-
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 import {
   Box,
@@ -152,8 +155,22 @@ const AnnouncementPage = () => {
 
   // Modal의 상태를 관리하는 state
   const [writeModalOpen, setWriteModalOpen] = useState(false);
-  const [lookModalOpen,setLookModalOpen] = useState(false);
+  const [lookModalOpen, setLookModalOpen] = useState(false);
+  const [updateModalOpen, setUpdateModalOpen] = useState(false);
 
+  //수정 내역 불러오기
+  const [title, setTitle] = useState(`공지사항 입니다.`);
+  const [content, setContent] =
+    useState(`안녕하세요. 소중한 고객 여러분께 알려드리는 공지사항이 있습니다. 최근의 업데이트로 인해 당사의 서비스가 더욱 원활하고 효율적으로 운영될 수 있도록 노력하고 있습니다. 새롭게 추가된 기능들과 향상된 사용자 경험을 통해 더욱 편리한 서비스를 제공하고자 합니다.
+  <br/>
+  <br/>
+    이번 업데이트로는 보안 강화 및 속도 개선에 중점을 두었습니다.고객님들의 개인 정보를 보호하기 위해 최신 보안 기술을 도입하여 더욱 안전한 환경을 제공하고 있습니다. 또한, 서비스의 속도를 향상시켜 더 빠르고 신속한 이용이 가능하도록 조치하였습니다.
+    <br/>
+    <br/>
+    우리는 항상 고객님들의 소중한 의견에 귀 기울이고 있습니다. 서비스 이용 중 발생하는 어떠한 문제나 피드백이 있다면 언제든지 고객센터를 통해 알려주시기 바랍니다. 고객님들의 의견을 토대로 더 나은 서비스를 제공하기 위해 끊임없이 노력하고 있습니다.
+    <br/>
+    <br/>
+    많은 관심과 협조를 부탁드리며, 더 나은 서비스로 찾아뵙겠습니다. 감사합니다.`);
 
   const handleWriteOpenModal = () => {
     setWriteModalOpen(true);
@@ -163,17 +180,22 @@ const AnnouncementPage = () => {
     setWriteModalOpen(false);
   };
 
-  
+  const handleUpdateOpenModal = () => {
+    setUpdateModalOpen(true);
+  };
+  const handleUpdateCloseModal = () => {
+    setUpdateModalOpen(false);
+    setLookModalOpen(false);
+  };
 
   const handleLookOpenModal = () => {
-    setLookModalOpen(true)
-  }
+    setLookModalOpen(true);
+  };
   const handleLookCloseModal = () => {
-    setLookModalOpen(false)
-  }
+    setLookModalOpen(false);
+  };
 
   return (
-    
     <Paper sx={{ display: "flex", height: "100vh" }}>
       {/* AdminBar 컴포넌트에 selectedMenu와 setSelectedMenu props 전달 */}
       <AdminBar selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu} />
@@ -261,20 +283,23 @@ const AnnouncementPage = () => {
 
           <Pagination count={10} />
           {/* 공지사항 작성 모달  */}
-          <Dialog onClose={handleWriteOpenModal} open={writeModalOpen} maxWidth={false}>
-            <DialogTitle> 
+          <Dialog
+            onClose={handleWriteOpenModal}
+            open={writeModalOpen}
+            maxWidth={false}
+          >
+            <DialogTitle>
               <InputBoxM
-              color="neutral"
-              placeholder="Text"
-              disabled={false}
-              variant="soft"
-              sx={{ mb: 2, mt: 2,width: '100%' }}
-             />
+                color="neutral"
+                placeholder="Text"
+                disabled={false}
+                variant="soft"
+                sx={{ mb: 2, mt: 2, width: "100%" }}
+              />
             </DialogTitle>
             <DialogContent style={{ width: 900, height: 550 }}>
-              
               <CKEditorContainer>
-              <CKEditor
+                <CKEditor
                   editor={ClassicEditor}
                   data="<p>공지를 작성하세요</p>"
                   onChange={(event, editor) => {
@@ -294,34 +319,100 @@ const AnnouncementPage = () => {
             </DialogContent>
           </Dialog>
 
+          {/* 공지사항 수정 모달  */}
+          <Dialog
+            onClose={handleUpdateCloseModal}
+            open={updateModalOpen}
+            maxWidth={false}
+          >
+            <DialogTitle
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <CustomSelect
+                size="s"
+                sx={{ marginRight: "10px" /* 추가적인 스타일 */ }}
+              />
+              <InputBoxM
+                value={title}
+                color="neutral"
+                placeholder="Text"
+                disabled={false}
+                variant="soft"
+                sx={{ mb: 2, mt: 2, width: "70%" }}
+              />
+            </DialogTitle>
+            <DialogContent style={{ width: 900, height: 550 }}>
+              <CKEditorContainer>
+                <CKEditor
+                  editor={ClassicEditor}
+                  data={content}
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    console.log({ event, editor, data });
+                    // 원하는 작업 수행
+                  }}
+                />
+              </CKEditorContainer>
+              <DialogActions
+                style={{ justifyContent: "center", marginTop: "20px" }}
+              >
+                <AdminButton autoFocus onClick={handleUpdateCloseModal}>
+                  Save
+                </AdminButton>
+              </DialogActions>
+            </DialogContent>
+          </Dialog>
+
           {/* 공지사항 보기 모달 */}
-          <Dialog onClose={handleLookOpenModal} open={lookModalOpen} maxWidth={false}>
+          <Dialog
+            onClose={handleLookOpenModal}
+            open={lookModalOpen}
+            maxWidth={false}
+          >
             <DialogContent style={{ width: 900, height: 600 }}>
               <div>
-                <Grid marginTop="2%" style={{textAlign:"center"}}>
-                   <h2>공지사항 입니다.</h2>
+                <Grid marginTop="2%" style={{ textAlign: "center" }}>
+                  <h2>공지사항 입니다.</h2>
                 </Grid>
-                <Grid>
-                  작성일시 : 2024-01-22  작성자 라이언
-                </Grid>
-                
-                <Typography style={{marginTop:"5%"}}>
-                안녕하세요. 소중한 고객 여러분께 알려드리는 공지사항이 있습니다. 최근의 업데이트로 인해 당사의 서비스가 더욱 원활하고 효율적으로 운영될 수 있도록 노력하고 있습니다. 새롭게 추가된 기능들과 향상된 사용자 경험을 통해 더욱 편리한 서비스를 제공하고자 합니다.
-                <br/>
-                <br/>
-                  이번 업데이트로는 보안 강화 및 속도 개선에 중점을 두었습니다.고객님들의 개인 정보를 보호하기 위해 최신 보안 기술을 도입하여 더욱 안전한 환경을 제공하고 있습니다. 또한, 서비스의 속도를 향상시켜 더 빠르고 신속한 이용이 가능하도록 조치하였습니다.
-                  <br/>
-                  <br/>
-                  우리는 항상 고객님들의 소중한 의견에 귀 기울이고 있습니다. 서비스 이용 중 발생하는 어떠한 문제나 피드백이 있다면 언제든지 고객센터를 통해 알려주시기 바랍니다. 고객님들의 의견을 토대로 더 나은 서비스를 제공하기 위해 끊임없이 노력하고 있습니다.
-                  <br/>
-                  <br/>
-                  많은 관심과 협조를 부탁드리며, 더 나은 서비스로 찾아뵙겠습니다. 감사합니다.
+                <Grid>작성일시 : 2024-01-22 작성자 라이언</Grid>
+
+                <Typography style={{ marginTop: "5%" }}>
+                  안녕하세요. 소중한 고객 여러분께 알려드리는 공지사항이
+                  있습니다. 최근의 업데이트로 인해 당사의 서비스가 더욱 원활하고
+                  효율적으로 운영될 수 있도록 노력하고 있습니다. 새롭게 추가된
+                  기능들과 향상된 사용자 경험을 통해 더욱 편리한 서비스를
+                  제공하고자 합니다.
+                  <br />
+                  <br />
+                  이번 업데이트로는 보안 강화 및 속도 개선에 중점을
+                  두었습니다.고객님들의 개인 정보를 보호하기 위해 최신 보안
+                  기술을 도입하여 더욱 안전한 환경을 제공하고 있습니다. 또한,
+                  서비스의 속도를 향상시켜 더 빠르고 신속한 이용이 가능하도록
+                  조치하였습니다.
+                  <br />
+                  <br />
+                  우리는 항상 고객님들의 소중한 의견에 귀 기울이고 있습니다.
+                  서비스 이용 중 발생하는 어떠한 문제나 피드백이 있다면 언제든지
+                  고객센터를 통해 알려주시기 바랍니다. 고객님들의 의견을 토대로
+                  더 나은 서비스를 제공하기 위해 끊임없이 노력하고 있습니다.
+                  <br />
+                  <br />
+                  많은 관심과 협조를 부탁드리며, 더 나은 서비스로
+                  찾아뵙겠습니다. 감사합니다.
                 </Typography>
               </div>
               <DialogActions
                 style={{ justifyContent: "center", marginTop: "40px" }}
               >
-                <AdminButton autoFocus onClick={handleLookCloseModal} style={{marginRight:"5%"}}>
+                <AdminButton
+                  autoFocus
+                  onClick={handleUpdateOpenModal}
+                  style={{ marginRight: "5%" }}
+                >
                   수정
                 </AdminButton>
                 <AdminButton autoFocus onClick={handleLookCloseModal}>
