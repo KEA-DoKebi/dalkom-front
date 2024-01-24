f67ef63fe446637974900f5f060dd905316a0b14
ms987796 <ms987796@naver.com>
2024-01-24 15:13:40 +0900

    [DOK-64] build: 안쓰는거 제거

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,5 +1,5 @@
 import { Paper } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Carousel from "react-material-ui-carousel";
 import styled from "styled-components";
 const StyledCarousel = styled(Carousel)`
