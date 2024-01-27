41268cb25a38634ff976dde42956356cd616c7b8
rhewjunior <fbckdals99@gmail.com>
2024-01-27 16:06:35 +0900

    dataList의 key값과 dataListLabels 매칭 완료

--- src/pages/Admin/InquiryManage/OrderInquiryPage.js
+++ src/pages/Admin/InquiryManage/OrderInquiryPage.js
@@ -1,103 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 import SearchIcon from "@mui/icons-material/Search";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import {
-  Box,
-  Divider,
-  Paper,
-  Toolbar,
-  Pagination,
-  List,
-  ListItem,
-  Typography,
-  IconButton,
-} from "@mui/material";
+import {Box, Divider, IconButton, List, ListItem, Pagination, Paper, Toolbar, Typography,} from "@mui/material";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import Modal from "@mui/material/Modal";
 import AdminBar from "components/organisms/AdminBar";
-import { MuiColorChip } from "components/atoms/AdminChip";
-import { InputBoxS } from "components/atoms/Input";
-import { AdminButton } from "components/atoms/AdminCommonButton";
+import {MuiColorChip} from "components/atoms/AdminChip";
+import {InputBoxS} from "components/atoms/Input";
+import {AdminButton} from "components/atoms/AdminCommonButton";
 import axios from "axios";
 
 const mockData = {
-  제목: "안녕하세요. 문의글입니다.",
-  내용: "문의 내용테스트 글 입니다.문의 내용테스트 글  내용테스트 글 입니다.",
-  사진: "src/images/benefit.png",
+    제목: "안녕하세요. 문의글입니다.",
+    내용: "문의 내용테스트 글 입니다.문의 내용테스트 글  내용테스트 글 입니다.",
+    사진: "src/images/benefit.png",
 };
-
-// const dataList = [
-//   {
-//     문의번호: "1001",
-//     문의일시: "2024-01-21 10:30",
-//     문의글: "주문주문주문주문주문주문주문주문주문주문주문주문주문주문주문",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1002",
-//     문의일시: "2024-01-20 15:20",
-//     문의글: "제품 교환 가능한가요?",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1003",
-//     문의일시: "2024-01-19 13:45",
-//     문의글: "환불 절차를 알고 싶습니다.",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1004",
-//     문의일시: "2024-01-18 17:00",
-//     문의글: "제품 사용 방법 문의합니다.",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1005",
-//     문의일시: "2024-01-17 09:30",
-//     문의글: "주문 취소하고 싶습니다.",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1006",
-//     문의일시: "2024-01-16 12:10",
-//     문의글: "배송 지연에 대한 문의",
-//     답변여부: "대기중",
-//   },
-//   {
-//     문의번호: "1007",
-//     문의일시: "2024-01-15 11:25",
-//     문의글: "색상 변경 가능한가요?",
-//     답변여부: "완료",
-//   },
-//   {
-//     문의번호: "1008",
-//     문의일시: "2024-01-14 14:55",
-//     문의글: "결제 수단 변경을 원합니다.",
-//     답변여부: "완료",
-//   },
-//   {
-//     문의번호: "1009",
-//     문의일시: "2024-01-13 16:40",
-//     문의글: "회원 정보 수정 관련 문의",
-//     답변여부: "완료",
-//   },
-//   {
-//     문의번호: "1010",
-//     문의일시: "2024-01-12 18:20",
-//     문의글: "추가 주문 관련 문의드립니다.",
-//     답변여부: "완료",
-//   },
-// ];
 
 const StyledList = styled(List)`
   /* Add styling for the List component */
   padding: 0;
   width: 100%;
   border: none; /* Remove border */
-  background-color: background.paper;
+  background-color: background .paper;
 `;
 
 const ListItemStyled = styled(ListItem)`
@@ -132,208 +59,214 @@ const ModalBoxStyled = styled(Box)`
 
 // 간격 일정하게 만드는 거
 const getColumnWidth = (label) => {
-  // Define your width ranges for each column label
-  const widthRanges = {
-    문의번호: [0, 10],
-    문의일시: [0, 10],
-    문의글: [0, 50],
-    답변여부: [0, 10],
-    상세보기: [0, 10],
-    // Add more labels as needed
-  };
-  const [minWidth, maxWidth] = widthRanges[label] || [0, 100];
-  const width = Math.min(100, maxWidth) - minWidth;
+    // Define your width ranges for each column label
+    const widthRanges = {
+        문의번호: [0, 10],
+        문의일시: [0, 10],
+        문의제목: [0, 50],
+        답변여부: [0, 10],
+        상세보기: [0, 15],
+        // Add more labels as needed
+    };
+    const [minWidth, maxWidth] = widthRanges[label] || [0, 100];
+    const width = Math.min(100, maxWidth) - minWidth;
 
-  return `calc(${width}% - 8px)`; // Adjust 8px for spacing
+    return `calc(${width}% - 8px)`; // Adjust 8px for spacing
 };
 
 const OrderInquiryPage = () => {
-  const [dataList, setDataList] = useState([]);
-  const dataListLabels = dataList.length > 0 ? ['문의 번호', '문의 일시', '문의 제목', '답변 여부'] : [];
-  // const dataListLabels = dataList.length > 0 ? Object.keys(dataList[0]) : [];
+    const [dataList, setDataList] = useState([]);
+    const [dataListLabels, setDataListLabels] = useState([]);
 
 
-  // Declare selectedMenu and setSelectedMenu using useState
-  const [selectedMenu, setSelectedMenu] = useState("");
+    // Declare selectedMenu and setSelectedMenu using useState
+    const [selectedMenu, setSelectedMenu] = useState("");
 
-  const getInquiryByCategory = async () => {
-    // 배송 문의 카테고리는 35번
-    const res = await axios.get("/api/inquiry/category/35?page=0&size=10");
-    console.log(res.data.result.data.content);
-    setDataList(res.data.result.data.content);
-  };
+    const getInquiryByCategory = async () => {
+        // 배송 문의 카테고리는 35번
+        const res = await axios.get("/api/inquiry/category/35?page=0&size=10");
+        console.log(res.data.result.data.content);
 
-  useEffect(() => {
-    // 각 페이지가 마운트될 때 selectedMenu를 업데이트
-    // setSelectedMenu 함수를 호출하여 상태를 업데이트
-    getInquiryByCategory();
-    setSelectedMenu("주문 문의");
-  }, []);
+        const mappedDataList = res.data.result.data.content.map(item => ({
+            '문의번호': item.inquirySeq,
+            '문의일시': item.createdAt,
+            '문의제목': item.title,
+            '답변여부': item.answerState === 'Y' ? 'completed' : 'waiting',
+        }));
 
-  const [openModal, setOpenModal] = useState(false);
-  const handleOpenModal = () => setOpenModal(true);
-  const handleCloseModal = () => setOpenModal(false);
+        setDataList(mappedDataList);
 
-  return (
-    <Paper sx={{ display: "flex", height: "100vh" }}>
-      {/* AdminBar 컴포넌트에 selectedMenu와 setSelectedMenu props 전달 */}
-      <AdminBar selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu} />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          backgroundColor: "#EEF2F6",
-          flexGrow: 1,
-        }}
-      >
-        <Toolbar />
-        <Box
-          component="main"
-          justifyContent="center"
-          alignItems="center"
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            backgroundColor: "#FFFFFF",
-            borderRadius: "27px",
-            margin: "16px",
-          }}
-        >
-          <Toolbar sx={{ justifyContent: "space-between", width: "100%" }}>
-            {/* 중앙 정렬을 위해 앞뒤로 <div/> 추가*/}
-            <div />
-            <InputBoxS
-              color="neutral"
-              disabled={false}
-              startDecorator={<SearchIcon />}
-              placeholder="Search"
-              variant="soft"
-              sx={{ mb: 4, mt: 4 }}
-            />
-            <div />
-          </Toolbar>
+        const newLabels = Object.keys(mappedDataList[0]);
+        setDataListLabels(newLabels);
+    };
 
-          <StyledList aria-label="mailbox folders">
-            <ListItemStyled>
-              {dataListLabels.map((label, index) => (
-                <React.Fragment key={index}>
-                  <Typography
-                    variant="h6"
-                    fontWeight="bold"
-                    sx={{ width: getColumnWidth(label), textAlign: "center" }}
-                  >
-                    {label}
-                  </Typography>
-                </React.Fragment>
-              ))}
-              <Typography variant="h6" fontWeight="bold">
-                주문문의
-              </Typography>
-            </ListItemStyled>
-            <Divider component="li" light />
-            {dataList.map((item, rowIndex) => (
-              <React.Fragment key={rowIndex}>
-                <ListItemStyled>
-                  {dataListLabels.map((label, colIndex) =>
-                    label === "답변여부" ? (
-                      // 재고에 대한 스핀박스 렌더링
-                      <MuiColorChip
-                        status={
-                          item["답변여부"] === "대기중"
-                            ? "waiting"
-                            : "completed"
-                        }
-                      />
-                    ) : (
-                      // 다른 데이터는 Typography로 렌더링
-                      <Typography
-                        variant="body1"
-                        key={colIndex}
-                        sx={{
-                          width: getColumnWidth(label),
-                          textAlign: "center",
-                        }}
-                      >
-                        {item[label]}
-                      </Typography>
-                    ),
-                  )}
+    useEffect(() => {
+        // 각 페이지가 마운트될 때 selectedMenu를 업데이트
+        // setSelectedMenu 함수를 호출하여 상태를 업데이트
+        getInquiryByCategory();
+        setSelectedMenu("주문 문의");
+    }, []);
 
-                  <IconButton onClick={handleOpenModal}>
-                    <InfoOutlinedIcon />
-                  </IconButton>
-                </ListItemStyled>
-                {rowIndex !== dataList.length - 1 && (
-                  <Divider component="li" light />
-                )}
-              </React.Fragment>
-            ))}
-          </StyledList>
+    const [openModal, setOpenModal] = useState(false);
+    const handleOpenModal = () => setOpenModal(true);
+    const handleCloseModal = () => setOpenModal(false);
 
-          <Pagination count={10} />
-
-          <Modal
-            open={openModal}
-            onClose={handleCloseModal}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-          >
-            <ModalBoxStyled>
-              <IconButton
-                onClick={handleCloseModal}
-                sx={{ mt: 4, mr: 4 }}
-                style={{ position: "absolute", right: 0, top: 0 }}
-              >
-                <HighlightOffIcon></HighlightOffIcon>
-              </IconButton>
-
-              <Grid container spacing={2}>
-                <Grid item xs={2}>
-                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                    제목
-                  </Typography>
-                </Grid>
-                <Grid item xs={10}>
-                  <Typography>{mockData.제목}</Typography>
-                </Grid>
-                <Grid item xs={2}>
-                  <Typography variant="h6" fontWeight="bold" sx={{ mb: 2 }}>
-                    내용
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs={10}
-                  style={{
-                    height: "600px",
-                    maxHeight: "400px",
-                    overflowY: "auto",
-                  }}
+    return (
+        <Paper sx={{display: "flex", height: "100vh"}}>
+            {/* AdminBar 컴포넌트에 selectedMenu와 setSelectedMenu props 전달 */}
+            <AdminBar selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu}/>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "flex-start",
+                    backgroundColor: "#EEF2F6",
+                    flexGrow: 1,
+                }}
+            >
+                <Toolbar/>
+                <Box
+                    component="main"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{
+                        flexGrow: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        backgroundColor: "#FFFFFF",
+                        borderRadius: "27px",
+                        margin: "16px",
+                    }}
                 >
-                  <Typography>{mockData.내용.repeat(100)}</Typography>
-                </Grid>
+                    <Toolbar sx={{justifyContent: "space-between", width: "100%"}}>
+                        {/* 중앙 정렬을 위해 앞뒤로 <div/> 추가*/}
+                        <div/>
+                        <InputBoxS
+                            color="neutral"
+                            disabled={false}
+                            startDecorator={<SearchIcon/>}
+                            placeholder="Search"
+                            variant="soft"
+                            sx={{mb: 4, mt: 4}}
+                        />
+                        <div/>
+                    </Toolbar>
 
-                <Grid item xs={12} style={{ height: "20" }}></Grid>
-              </Grid>
+                    <StyledList aria-label="mailbox folders">
+                        <ListItemStyled>
+                            {dataListLabels.map((label, index) => (
+                                <React.Fragment key={index}>
+                                    <Typography
+                                        variant="h6"
+                                        fontWeight="bold"
+                                        sx={{width: getColumnWidth(label), textAlign: "center"}}
+                                    >
+                                        {label}
+                                    </Typography>
+                                </React.Fragment>
+                            ))}
+                            <Typography variant="h6" fontWeight="bold">
+                                주문문의
+                            </Typography>
+                        </ListItemStyled>
+                        <Divider component="li" light/>
+                        {dataList.map((item, rowIndex) => (
+                            <React.Fragment key={rowIndex}>
+                                <ListItemStyled>
+                                    {dataListLabels.map((label, colIndex) =>
+                                        label === "답변여부" ? (
+                                            // 재고에 대한 스핀박스 렌더링
+                                            <MuiColorChip
+                                                status={item["답변여부"]}
+                                            />
+                                        ) : (
+                                            // 다른 데이터는 Typography로 렌더링
+                                            <Typography
+                                                variant="body1"
+                                                key={colIndex}
+                                                sx={{
+                                                    width: getColumnWidth(label),
+                                                    textAlign: "center",
+                                                }}
+                                            >
+                                                {item[label]}
+                                            </Typography>
+                                        ),
+                                    )}
 
-              <TextField
-                id="outlined-textarea"
-                label="문의답변을 입력하세요"
-                placeholder="Placeholder"
-                maxrows={4}
-                rows={4}
-                multiline
-                sx={{ mb: 4, width: "100%", backgroundColor: "#f8fafc" }}
-              />
-              <AdminButton variant="contained">저장</AdminButton>
-            </ModalBoxStyled>
-          </Modal>
-        </Box>
-      </Box>
-    </Paper>
-  );
+                                    <IconButton onClick={handleOpenModal}>
+                                        <InfoOutlinedIcon/>
+                                    </IconButton>
+                                </ListItemStyled>
+                                {rowIndex !== dataList.length - 1 && (
+                                    <Divider component="li" light/>
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </StyledList>
+
+                    <Pagination count={10}/>
+
+                    <Modal
+                        open={openModal}
+                        onClose={handleCloseModal}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                    >
+                        <ModalBoxStyled>
+                            <IconButton
+                                onClick={handleCloseModal}
+                                sx={{mt: 4, mr: 4}}
+                                style={{position: "absolute", right: 0, top: 0}}
+                            >
+                                <HighlightOffIcon></HighlightOffIcon>
+                            </IconButton>
+
+                            <Grid container spacing={2}>
+                                <Grid item xs={2}>
+                                    <Typography variant="h6" fontWeight="bold" sx={{mb: 2}}>
+                                        제목
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={10}>
+                                    <Typography>{mockData.제목}</Typography>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <Typography variant="h6" fontWeight="bold" sx={{mb: 2}}>
+                                        내용
+                                    </Typography>
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={10}
+                                    style={{
+                                        height: "600px",
+                                        maxHeight: "400px",
+                                        overflowY: "auto",
+                                    }}
+                                >
+                                    <Typography>{mockData.내용.repeat(100)}</Typography>
+                                </Grid>
+
+                                <Grid item xs={12} style={{height: "20"}}></Grid>
+                            </Grid>
+
+                            <TextField
+                                id="outlined-textarea"
+                                label="문의답변을 입력하세요"
+                                placeholder="Placeholder"
+                                maxrows={4}
+                                rows={4}
+                                multiline
+                                sx={{mb: 4, width: "100%", backgroundColor: "#f8fafc"}}
+                            />
+                            <AdminButton variant="contained">저장</AdminButton>
+                        </ModalBoxStyled>
+                    </Modal>
+                </Box>
+            </Box>
+        </Paper>
+    );
 };
 
 export default OrderInquiryPage;
