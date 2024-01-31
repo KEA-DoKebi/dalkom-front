03a4b4090610d4ae4fa9d51b68675bb47f7375b1
juhye87 <78808933+juhye87@users.noreply.github.com>
2024-01-31 11:45:42 +0900

    build : 핑크 스위치 수정

--- src/components/atoms/OnOffSwitch.js
+++ src/components/atoms/OnOffSwitch.js
@@ -3,7 +3,7 @@ import { pink } from "@mui/material/colors";
 import styled from "styled-components";
 
 // On/Off 스위치 - (OnOffSwitch) 가져다가 쓰면 됩니당
-export const PinkSwitch = styled(Switch)(({ }) => ({
+export const PinkSwitch = styled(Switch)(() => ({
   "& .MuiSwitch-switchBase.Mui-checked": {
     color: pink[600],
     "&:hover": {
