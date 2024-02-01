22ef4f30e5bfea099216f35ec8a5761f5227a577
김민준 <75360915+minjun0707@users.noreply.github.com>
2024-02-01 13:55:42 +0900

    [Deploy] 배포 설정 변경 (#151)
    * fix : api base url 설정 변경
    
    * fix : proxy 미들웨어 로직 수정
    
    프로덕션 환경에서는 프록시 미들웨어 설정을 하지 않도록 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,22 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: process.env.REACT_APP_SERVER_ADDRESS,
-      withCredentials: true,
-      changeOrigin: true,
-    }),
-  );
+  if (process.env.REACT_APP_NODE_ENV === 'development') {
+    app.use(
+      createProxyMiddleware("/api", {
+        target: process.env.REACT_APP_SERVER_ADDRESS,
+        withCredentials: true,
+        changeOrigin: true,
+      }),
+    );
 
-  app.use(
-    createProxyMiddleware("/redis", {
-      target: process.env.REACT_APP_SERVER_ADDRESS,
-      withCredentials: true,
-      changeOrigin: true,
-    }),
-  );
+    app.use(
+      createProxyMiddleware("/redis", {
+        target: process.env.REACT_APP_SERVER_ADDRESS,
+        withCredentials: true,
+        changeOrigin: true,
+      }),
+    );
+  }
 };
+
