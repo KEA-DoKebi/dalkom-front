1872381299b73f51b91d2390c7a4c37a6bb0820e
rhewjunior <fbckdals99@gmail.com>
2024-02-04 02:06:04 +0900

    [DOK-316] design : 취소 반품 교환 환불 완료

--- src/pages/User/MyPage/Order/RefundPage/RefundBody.js
+++ src/pages/User/MyPage/Order/RefundPage/RefundBody.js
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import {
-  Paper,
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import { TokenAxios } from "apis/CommonAxios";
+import {Box, Pagination, Paper, Table, TableBody, TableCell, TableHead, TableRow, Typography,} from "@mui/material";
+import {TokenAxios} from "apis/CommonAxios";
 
 const Img = styled.img`
   width: 70px;
@@ -41,143 +33,140 @@ const StyledTableRow = styled(TableRow)`
 `;
 
 export default function RefundBody() {
-  const [data, setData] = useState([]);
-  const refundHistory = async () =>{
-    try{
-      const res = await TokenAxios.get("/api/order/canclerefundlist?page=0&size=2");
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState();
 
-      setData(res.data.result.data.content);
-    }catch(e){
-      console.log(e);
+    const refundHistory = async (page) => {
+        try {
+            const res = await TokenAxios.get(`/api/order/cancelrefundlist?page=${page}&size=10`);
+            setTotalPages(res.data.result.data.totalPages);
+            setData(res.data.result.data.content);
+        } catch (e) {
+            console.log(e);
+        }
     }
-  }
 
-  useEffect(() => {
-    refundHistory();
-  },[])
+    useEffect(() => {
+        refundHistory(currentPage);
+    }, [currentPage])
 
-  // const [data, setdata] = useState([]); // 상품 데이터 상태를 빈 배열로 초기화
+    const handlePageChange = (event, newPage) => {
+        setCurrentPage(newPage); // 현재 페이지 업데이트
+    };
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get("/data/PeopleManage/MyRefund.json");
-  //       // 데이터 변환 과정에서 상태 값을 확인하여 문자열 변환
-  //       const RefundData = response.data.RefundData.map((item) => ({
-  //         name: item.productName,
-  //         option: item.option,
-  //         imageUrl: item.imageUrl,
-  //         type: item.type,
-  //         state: item.state === "Y" ? "완료" : "처리중",
-  //         RefundDate: item.Date,
-  //       }));
-  //       setdata(RefundData);
-  //     } catch (error) {
-  //       console.error("Failed to fetch data:", error);
-  //     }
-  //   };
+    return (
+        <Paper elevation={0}>
+            <Typography sx={{fontSize: "40px", mb: "3vh"}}>
+                취소/반품/교환/환불
+            </Typography>
 
-  //   fetchData();
-  // }, []);
-
-  return (
-    <Paper>
-      <Typography sx={{ fontSize: "40px", mb: "3vh" }}>
-        취소/반품/교환/환불
-      </Typography>
-
-      <Paper
-        elevation={0}
-        style={{ display: "flex", justifyContent: "center", width: "100%" }}
-      >
-        <TableContainer>
-          <Table
-            sx={{ border: "1px solid", borderColor: "#e0e0e0", margin: "auto" }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  style={{
-                    width: "25%",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "15px",
-                  }}
-                >
-                  상품명(옵션)
-                </TableCell>
-                <TableCell
-                  style={{
-                    width: "25%",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "15px",
-                  }}
-                >
-                  신청일자
-                </TableCell>
-                <TableCell
-                  style={{
-                    width: "25%",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "15px",
-                  }}
-                >
-                  구분
-                </TableCell>
-                <TableCell
-                  style={{
-                    width: "25%",
-                    textAlign: "center",
-                    fontWeight: "bold",
-                    fontSize: "15px",
-                  }}
-                >
-                  상태
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.length > 0
-                ? data.map((row, index) => (
-                    <StyledTableRow key={index}>
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{ width: "25%", alignItems: "center" }}
-                      >
-                        <ProductInfo>
-                          <Img src={row.imageUrl} />
-                          <TextContainer>
-                            <div>{row.productName}</div>
-                            <div style={{ marginTop: "4px" }}>{row.optionDetail}</div>
-                          </TextContainer>
-                        </ProductInfo>
-                      </TableCell>
-                      <TableCell style={{ width: "25%", textAlign: "center" }}>
-                        {row.modifiedAt.substring(1,10)}
-                      </TableCell>
-                      <TableCell style={{ width: "25%", textAlign: "center" }}>
-                        {row.requestType}
-                      </TableCell>
-                      <TableCell style={{ width: "25%", textAlign: "center" }}>
-                        {row.requestState}
-                      </TableCell>
-                    </StyledTableRow>
-                  ))
-                : [...Array(4)].map((_, index) => (
-                    <StyledTableRow key={`empty-${index}`}>
-                      <TableCell
-                        colSpan={4}
-                        style={{ height: "110px", borderBottom: "none" }}
-                      />
-                    </StyledTableRow>
-                  ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Paper>
-  );
+            <Paper elevation={0}
+                style={{display: "flex", justifyContent: "center", width: "100%"}}
+            >
+                <TableContainer>
+                    <Table
+                        sx={{border: "1px solid", borderColor: "#e0e0e0", margin: "auto"}}
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell
+                                    style={{
+                                        width: "25%",
+                                        textAlign: "center",
+                                        fontWeight: "bold",
+                                        fontSize: "15px",
+                                    }}
+                                >
+                                    상품명(옵션)
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        width: "25%",
+                                        textAlign: "center",
+                                        fontWeight: "bold",
+                                        fontSize: "15px",
+                                    }}
+                                >
+                                    신청일자
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        width: "25%",
+                                        textAlign: "center",
+                                        fontWeight: "bold",
+                                        fontSize: "15px",
+                                    }}
+                                >
+                                    구분
+                                </TableCell>
+                                <TableCell
+                                    style={{
+                                        width: "25%",
+                                        textAlign: "center",
+                                        fontWeight: "bold",
+                                        fontSize: "15px",
+                                    }}
+                                >
+                                    상태
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {data.length > 0
+                                ? data.map((row, index) => (
+                                    <StyledTableRow key={index}>
+                                        <TableCell
+                                            component="th"
+                                            scope="row"
+                                            style={{width: "25%", alignItems: "center"}}
+                                        >
+                                            <ProductInfo>
+                                                <Img src={row.imageUrl}/>
+                                                <TextContainer>
+                                                    <div>{row.productName}</div>
+                                                    <div style={{marginTop: "4px"}}>{row.optionDetail}</div>
+                                                </TextContainer>
+                                            </ProductInfo>
+                                        </TableCell>
+                                        <TableCell style={{width: "25%", textAlign: "center"}}>
+                                            {row.modifiedAt.substring(1, 10)}
+                                        </TableCell>
+                                        <TableCell style={{width: "25%", textAlign: "center"}}>
+                                            {row.requestType}
+                                        </TableCell>
+                                        <TableCell style={{width: "25%", textAlign: "center"}}>
+                                            {row.requestState}
+                                        </TableCell>
+                                    </StyledTableRow>
+                                ))
+                                : [...Array(4)].map((_, index) => (
+                                    <StyledTableRow key={`empty-${index}`}>
+                                        <TableCell
+                                            colSpan={4}
+                                            style={{height: "110px", borderBottom: "none"}}
+                                        />
+                                    </StyledTableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+            <Box
+                sx={{
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: "2.5%"
+                }}
+            >
+                <Pagination
+                    count={totalPages} // 총 페이지 수를 적용
+                    page={currentPage + 1} // 현재 페이지 설정 (0부터 시작하므로 그대로 사용)
+                    onChange={(event, newPage) => handlePageChange(event, newPage - 1)} // 페이지 변경 시 호출되는 함수 설정
+                />
+            </Box>
+        </Paper>
+    );
 }
